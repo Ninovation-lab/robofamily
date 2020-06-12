cd47c7f252b7ebdb9fcc3af6c469ed4caa99f9fd
Ninovation-lab <ninovationlab@gmail.com>
2020-06-12 20:40:46 +0200

    Uploading my first web APP with React

---
+++ precache-manifest.9f541876498b742aa82d76e02f07446a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19731ce074b70d0560fc86f94aa6c7f9",
+    "url": "/robofamily/index.html"
+  },
+  {
+    "revision": "81aab55deb7262b4be73",
+    "url": "/robofamily/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6b49820c7fa9202a4893",
+    "url": "/robofamily/static/css/main.5180b67f.chunk.css"
+  },
+  {
+    "revision": "81aab55deb7262b4be73",
+    "url": "/robofamily/static/js/2.91c91532.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofamily/static/js/2.91c91532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b49820c7fa9202a4893",
+    "url": "/robofamily/static/js/main.8740215e.chunk.js"
+  },
+  {
+    "revision": "66dd584b64048c452171",
+    "url": "/robofamily/static/js/runtime-main.68013d99.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofamily/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "bf9bd1ec93bfc520cee4161aaa8dfa83",
+    "url": "/robofamily/static/media/ninovation.bf9bd1ec.png"
+  }
+]);
